453ff499dde1a56cd1937e330f1cbea3161339ac
lhw414 <dlgusdn0414@snu.ac.kr>
2024-07-24 05:27:28 +0000

    fix bfp16 error

--- src/solver/var/backward_var.cpp
+++ src/solver/var/backward_var.cpp
@@ -87,7 +87,7 @@ ConvSolution VarBackward::GetSolution(const ExecutionContext& context,
             {"MIOPEN_USE_FP16", static_cast<int>(dtype == miopenHalf)},
             {"MIOPEN_USE_FP32", static_cast<int>(dtype == miopenFloat)},
             {"MIOPEN_USE_FP64", static_cast<int>(dtype == miopenDouble)},
-            {"MIOPEN_USE_BF16", static_cast<int>(dtype == miopenBFloat16)},
+            {"MIOPEN_USE_BFP16", static_cast<int>(dtype == miopenBFloat16)},
         };
 
         kernel.comp_options = build_params.GenerateFor(kbp::HIP{});
